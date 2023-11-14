f6abd057b23a36ca9f6b0470bf64a302109e4385
Bruno Gomes <brunomiguelgomes.bg8@gmail.com>
2023-11-14 12:51:54 +0000

    question repo, services and tests - update

---
+++ SmartMetric.Core/Services/Adders/QuestionAdderService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using SmartMetric.Core.Domain.Entities;
+using SmartMetric.Core.Domain.RepositoryContracts;
+using SmartMetric.Core.DTO.AddRequest;
+using SmartMetric.Core.DTO.Response;
+using SmartMetric.Core.Helpers;
+using SmartMetric.Core.ServicesContracts.Adders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMetric.Core.Services.Adders
+{
+    public class QuestionAdderService : IQuestionAdderService
+    {
+        private readonly IQuestionRepository _questionRepository;
+        private readonly ILogger<QuestionAdderService> _logger;
+
+        public QuestionAdderService(IQuestionRepository questionRepository, ILogger<QuestionAdderService> logger)
+        {
+            _questionRepository = questionRepository;
+            _logger = logger;
+        }
+
+        public async Task<QuestionDTOResponse> AddQuestion(QuestionDTOAddRequest? request)
+        {
+            _logger.LogInformation($"{nameof(QuestionAdderService)}.{nameof(AddQuestion)} foi iniciado");
+
+            if ( request == null ) { throw new ArgumentNullException(nameof(request)); }
+            ValidationHelper.ModelValidation(request);
+            Question translation = request.ToQuestion();
+            translation.QuestionId = Guid.NewGuid();
+            await _questionRepository.AddQuestion(translation);
+            return translation.ToQuestionDTOResponse();
+        }
+    }
+}
---
+++ SmartMetric.Core/ServicesContracts/Getters/IQuestionGetterService.cs
@@ -0,0 +1,27 @@
+﻿using SmartMetric.Core.DTO.Response;
+
+namespace SmartMetric.Core.ServicesContracts.Getters
+{
+    public interface IQuestionGetterService
+    {
+        /// <summary>
+        /// Procura todas as questões
+        /// </summary>
+        /// <returns>Retorna uma lista de objetos do tipo QuestionDTOResponse</returns>
+        Task<List<QuestionDTOResponse>> GetAllQuestion();
+
+        /// <summary>
+        /// Procura por uma questão através do seu Id passado por parâmetro
+        /// </summary>
+        /// <param name="questionId"></param>
+        /// <returns>Retorna um objeto do tipo QuestionDTOResponse</returns>
+        Task<QuestionDTOResponse?> GetQuestionById(Guid? questionId);
+
+        /// <summary>
+        /// Procura por todas as questões associadas ao formTemplate pretendido
+        /// </summary>
+        /// <param name="formTemplateId"></param>
+        /// <returns>Retorna uma lista de objetos do tipo QuestionDTOResponse</returns>
+        Task<List<QuestionDTOResponse>?> GetQuestionByFormTemplateId(Guid? formTemplateId);
+    }
+}
